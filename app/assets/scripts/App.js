765991def31416897082cad69a084d8169ec4855
jasonduquain <jduquain@gmail.com>
2017-10-05 08:34:13 -0400

    completed desktop and mobile hdr styles

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,45 @@
+/******MobileMenu.js*******/
+
+class MobileMenu {
+    constructor() {
+        this.siteHeader = document.querySelector('.site-header');
+        this.menuIcon = document.querySelector('.site-header__menu-icon');
+        this.menuContent = document.querySelector('.site-header__menu-content');
+        this.events();
+    }
+    
+    events() {
+        this.menuIcon.addEventListener('click', () => {
+            this.menuContent.classList.toggle('site-header__menu-content--is-visible');
+            this.siteHeader.classList.toggle('site-header--is-expanded');
+            this.menuIcon.classList.toggle('site-header__menu-icon--close-x');
+            
+        });
+    }
+}
+
+
+var mobileMenu = new MobileMenu();
+
+
+
+
+
+
+/******** RevealOnScroll.js********/
+
+
+
+
+
+
+
+/******* StickyHeader.js************/
+
+
+
+
+
+
+
+/************** Modal.js ***********/
